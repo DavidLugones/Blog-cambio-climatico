4b0848e33e88ff1864bb30f151db17a067236a5a
DavidLugones <davidlugonesfederico@gmail.com>
2023-12-21 20:22:12 -0300

    se agregó 3 noticias y el código inical de mostrar últimas noticias

--- apps/noticias/views.py
+++ apps/noticias/views.py
@@ -2,49 +2,48 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-
 from .models import Noticia
 from .forms import Formulario_Noticia, Formulario_Modificar_Noticia
 
-# Create your views here.
 def Home_Noticias(request):
-
-	todas = Noticia.objects.all()
-
-	contexto = {}
-
-	contexto ['noticias'] = todas
-
-	contexto ['fecha'] = '10-12-2023'
-	
-	return render(request, 'noticias/home_noticias.html', contexto)
+    todas = Noticia.objects.all()
+    contexto = {}
+    contexto['noticias'] = todas
+    contexto['fecha'] = '10-12-2023'
+    return render(request, 'noticias/home_noticias.html', contexto)
 
 class Home_Noticias_clase(ListView):
-	model = Noticia
-	template_name = 'noticias/home_noticias.html'
-	context_object_name = 'noticias'
+    model = Noticia
+    template_name = 'noticias/home_noticias.html'
+    context_object_name = 'noticias'
 
 class Cargar_noticia(CreateView):
-	model = Noticia
-	template_name = 'noticias/cargar_noticia.html'
-	form_class = Formulario_Noticia
-	success_url = reverse_lazy('noticias:h_noticias')
-
+    model = Noticia
+    template_name = 'noticias/cargar_noticia.html'
+    form_class = Formulario_Noticia
+    success_url = reverse_lazy('noticias:h_noticias')
 
 def Detalle_noticia(request, pk):
-	ctx = {}
-	n = Noticia.objects.get(pk = pk)
-	ctx['noticia'] = n
-	return render(request,'noticias/detalle_noticia.html', ctx)
+    ctx = {}
+    n = Noticia.objects.get(pk=pk)
+    ctx['noticia'] = n
+    return render(request,'noticias/detalle_noticia.html', ctx)
 
 class Modificar_noticia(UpdateView):
-	model = Noticia
-	template_name = 'noticias/modificar_noticia.html'
-	form_class = Formulario_Modificar_Noticia
-	success_url = reverse_lazy('noticias:h_noticias')
+    model = Noticia
+    template_name = 'noticias/modificar_noticia.html'
+    form_class = Formulario_Modificar_Noticia
+    success_url = reverse_lazy('noticias:h_noticias')
 
 class Borrar_noticia(DeleteView):
-	model = Noticia
-	success_url = reverse_lazy('noticias:h_noticias')
+    model = Noticia
+    success_url = reverse_lazy('noticias:h_noticias')
+
+def Home_Noticias_ultimas(request):
+    noticias = Noticia.objects.order_by('-fecha')[:3] 
+    return render(request, 'home.html', {'noticias': noticias})
+
+
+
 
 
