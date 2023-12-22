14f8297e187e34fa88f09cee9a962f716c5d10b4
pablopassero <ivan_pass01@homtail.com>
2023-12-22 14:04:47 -0300

    app comentarios, filtro por categoría y modificacion usuarios

--- apps/noticias/views.py
+++ apps/noticias/views.py
@@ -2,23 +2,58 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-
-from .models import Noticia
+from .models import Noticia, Categoria
 from .forms import Formulario_Noticia, Formulario_Modificar_Noticia
 
-# Create your views here.
+from apps.comentarios.models import Comentario
+
+#CONTROLA SI EL USUARIO ESTA LOGEADO EN UNA VISTA BASADA EN CLASES
+from django.contrib.auth.mixins import LoginRequiredMixin
+#CONTROLA SI EL USUARIO ESTA LOGEADO EN UNA VISTA BASADA EN FUNCIONEs
+from django.contrib.auth.decorators import login_required
+
+#CONTROLA QUE EL USUARIO SEA STAFF VISTA BASADA EN FuNCION
+from django.contrib.admin.views.decorators import staff_member_required
+#CONTROLA QUE EL USUARIO SEA STAFF PARA VISTA BASADA EN CLASE
+from django.contrib.auth.mixins    import UserPassesTestMixin
+
+
+#Vasta Basada en funciones
 def Home_Noticias(request):
-
-	todas = Noticia.objects.all()
-
 	contexto = {}
+	cat = Categoria.objects.all()
+	contexto['categorias'] = cat
 
-	contexto ['noticias'] = todas
+	#CAPTURAR FILTROS 
+	filtro = request.GET.get('categoria',None)
+	orden = request.GET.get('orden',None)
 
-	contexto ['fecha'] = '10-12-2023'
-	
+	if not filtro or filtro == '0':
+		todas = Noticia.objects.all()
+	else:
+		categoria_seleccionada = Categoria.objects.get(pk = filtro)
+		todas = Noticia.objects.filter(categoria = categoria_seleccionada)
+
+	if orden=="asc":
+		todas = todas.order_by('creado')
+	elif orden == 'dsc':
+		todas = todas.order_by('-creado')
+
+	contexto['noticias'] = todas
 	return render(request, 'noticias/home_noticias.html', contexto)
 
+#ACLARACION
+'''
+si bien en la vista armo un diccionario del tipo
+{noticias: todas_noticias, fecha: '28-11-2023'}
+en el template resivo variables separadas, una por cada clave, la cual contiene como valor
+el valor de la clave
+EJ, recibo 2 variales distintas, cuyo nombre es igual a la clave
+noticias = todas_noticias
+fecha = '28-11-2023'
+'''
+
+#VISTA BASADA EN CLASE
 class Home_Noticias_clase(ListView):
 	model = Noticia
 	template_name = 'noticias/home_noticias.html'
@@ -29,13 +64,11 @@ class Cargar_noticia(CreateView):
 	template_name = 'noticias/cargar_noticia.html'
 	form_class = Formulario_Noticia
 	success_url = reverse_lazy('noticias:h_noticias')
-
-
-def Detalle_noticia(request, pk):
-	ctx = {}
-	n = Noticia.objects.get(pk = pk)
-	ctx['noticia'] = n
-	return render(request,'noticias/detalle_noticia.html', ctx)
+	
+	def form_valid(self, form):
+		noticia = form.save(commit=False)
+		noticia.usuario = self.request.user
+		return super(Cargar_noticia, self).form_valid(form)
 
 class Modificar_noticia(UpdateView):
 	model = Noticia
@@ -48,3 +81,11 @@ class Borrar_noticia(DeleteView):
 	success_url = reverse_lazy('noticias:h_noticias')
 
 
+def Detalle_noticia(request, pk):
+	ctx = {}
+	n = Noticia.objects.get(pk = pk)
+	ctx['noticia'] = n
+
+	com = Comentario.objects.filter(noticia = n)
+	ctx['comentarios'] = com
+	return render(request,'noticias/detalle_noticia.html', ctx)
